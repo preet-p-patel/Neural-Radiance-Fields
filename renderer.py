1f6fc78308104f14e24cda40e7961f3f9a9a33d9
preet-p-patel <pppatel4212@gmail.com>
2025-03-09 19:38:31 -0400

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -43,7 +43,8 @@ class VolumeRenderer(torch.nn.Module):
         print("rays_feature shape:", rays_feature.shape)
         N = weights.shape[1]
         rf = rays_feature
-        rf.view(-1, N, 3)
+        rf = rf.view(-1, N, 3)
+        print("rf shape:", rf.shape)
         # N = rays_feature.shape[0]
         # weights = weights.view(N, -1, 1)
         feature = torch.sum(weights * rf, dim=1)
