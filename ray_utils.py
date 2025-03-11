495460dc2370543e5cdceb93816ff129a9896d85
preet-p-patel <pppatel4212@gmail.com>
2025-03-11 01:34:48 -0400

    Update ray_utils.py

--- ray_utils.py
+++ ray_utils.py
@@ -118,11 +118,11 @@ def get_random_pixels_from_image(n_pixels, image_size, camera):
     pass
 
     indices = torch.randperm(xy_grid.shape[0])
-    indices = indices.to(xy_grid.device)
+    indices = indices.to('cuda')
     xy_grid_sub = xy_grid[indices]
-    xy_grid_sub = xy_grid_sub.to(xy_grid.device)
-    # print("indices: ", indices.device)
-    # print("xy_grid_sub: ", xy_grid_sub.device)
+    xy_grid_sub = xy_grid_sub.to('cuda')
+    print("indices: ", indices.device)
+    print("xy_grid_sub: ", xy_grid_sub.device)
     # Return
     return xy_grid_sub.reshape(-1, 2)[:n_pixels]
 
